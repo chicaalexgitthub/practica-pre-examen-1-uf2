4d4ce25065068101d9ac650cd85be1f870a99f02
chicaalexgitthub <chica_alex_gitthub@gmx.com>
2022-12-20 20:25:19 +0100

    20 des, 20:25

---
+++ funcion 2.py
@@ -0,0 +1,12 @@
+def new_tfn():
+    try:
+        tfn = input("Introduce the new tfn: ")
+        if not tfn.isdigit():
+            raise ValueError("The tfn must be all digits.")
+        elif not len(tfn) == 9:
+            raise ValueError("The tfn must be 9 digits long.")
+        return tfn
+    except ValueError as error:
+        print(error)
+        input("Press enter to continue.")
+        new_tfn()
